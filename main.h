32daec81cf5f8ff041e8c296372fa172d02e8302
Amilemia <aminemadah2@gmail.com>
2023-04-23 10:49:40 +0100

    Header-file

---
+++ main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct fmt - Struct for formatting data.
+ * @sym: *sym: Pointer to a character string.
+ * @fn: func pointer to a function that takes a va_list as an arg.
+ */
+typedef struct fmt
+{
+	char *sym;
+	int (*fn)(va_list);
+} fmt_t;
+
+/* main */
+int _printf(const char *format, ...);
+
+/* print handler */
+int (*get_print(const char *format))(va_list);
+
+/* print_chars */
+int print_char(va_list list);
+int print_str(va_list list);
+int print_perc(va_list list);
+
+/* print_numbers */
+int print_int(va_list list);
+int print_unsigned(va_list list);
+
+/* print_bases */
+int print_binary(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+
+/* print_others */
+int print_rot13(va_list list);
+
+/* utilities */
+unsigned int _strlen(char *s);
+void reverse_str(char s[]);
+void _itoa(long n, char s[]);
+int to_base_n(unsigned int num, int base, char s[]);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* writes */
+int _putchar(char c);
+int _puts(char *str);
+
+#endif
