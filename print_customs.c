97f9926be4020e7bfe64b98d75bc5338b2d18236
Amilemia <aminemadah2@gmail.com>
2023-04-24 23:23:44 +0100

    Custom Task %r

--- print_customs.c
+++ print_customs.c
@@ -31,3 +31,28 @@ int print_STR(va_list list)
 	}
 	return (count);
 }
+
+/**
+ * print_rev - prints a string in reverse order
+ * @list: the arguments list passed to va_start()
+ * Return: the number of characters printed to the stdout stream
+ */
+int print_rev(va_list list)
+{
+	char *str, buffer[50];
+	int i;
+
+	str = va_arg(list, char *);
+
+	if (str == NULL)
+		return (_puts("(null)"));
+
+	for (i = 0; str[i]; i++)
+		buffer[i] = str[i];
+
+	buffer[i] = '\0';
+
+	reverse_str(buffer);
+
+	return (_puts(buffer));
+}
