5ea6589433130209546ef0bd21644698ac2fbd9c
amasin76 <amasin76@gmail.com>
2023-04-25 19:32:02 +0000

    Debugging: flags

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	va_list args, args_copy;
 	flags_t flags = {0};
 	int (*pfn)(va_list);
-	int i = 0, j, printed = 0, num, field_width;
+	int i = 0, printed = 0, num;
 
 	if (!format)
 		return (-1);
@@ -27,12 +27,13 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i++ + 1] == '\0')
+			if (format[i + 1] == '\0')
 				return (-1);
 			/* flag handler */
 			num = va_arg(args_copy, long);
 			parse_flags(format, &flags, num, &printed, &i);
 			/* handle field width */
+			/*
 			if (_isdigit(format[i + 1]))
 			{
 				field_width = format[i + 1] - '0';
@@ -40,6 +41,7 @@ int _printf(const char *format, ...)
 					field_width = field_width * 10 + (format[j] - '0');
 				i = j - 1;
 			}
+			*/
 			if (!whitespaces(format, &i))
 				return (-1);
 			pfn = get_print(&format[i]);
