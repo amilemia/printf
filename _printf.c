31a305e21fe65afd12af9b47115880fc9173ad14
amasin76 <amasin76@gmail.com>
2023-04-25 16:34:07 +0000

    Rollback

--- _printf.c
+++ _printf.c
@@ -17,9 +17,7 @@ int _printf(const char *format, ...)
 	int (*pfn)(va_list);
 	int i = 0, j, printed = 0, num, field_width;
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (!format)
 		return (-1);
 	va_start(args, format);
 	va_copy(args_copy, args);
@@ -35,9 +33,16 @@ int _printf(const char *format, ...)
 					field_width = field_width * 10 + (format[j] - '0');
 				i = j - 1;
 			}
+			/* if the string ends with a %, returns -1 */
+			if (format[i + 1] == '\0')
+				return (-1);
+			/* skip spaces between the % and the format specifier ex: "%  s" */
+			for (; format[i + 1] == ' '; i++)
+				if (format[i + 2] == '\0')
+					return (-1);
 			num = va_arg(args_copy, long);
 			parse_flags(format, &flags, num, &printed, &i);
-			pfn = get_print(&format[++i]);
+			pfn = get_print(&format[i]);
 			/* for invalid formats: print as is */
 			printed += pfn ? pfn(args) : _putchar('%') + _putchar(format[i]);
 		}
