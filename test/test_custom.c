f2e131ebc05e8c927a5cfb287d5d4f483f27afff
Amilemia <aminemadah2@gmail.com>
2023-04-26 02:55:39 +0100

    test file

--- test/test_custom.c
+++ test/test_custom.c
@@ -46,5 +46,41 @@ int main(void)
 	len_2 = _printf("%r\n", NULL);
 	printf("String len: %d\n", len_2);
 
+	/* ===========> %R <=========== */
+	printf("----------------------\n");
+	printf("---> R (13) CASES <---\n");
+	printf("----------------------\n");
+
+	/*Test case for NULL string input:*/
+	len_1 = printf("Expected output: %s\n", NULL);
+	len_2 = _printf("Current output:  %R\n", NULL);
+	printf("Expected length: [%d]\n", len_1);
+	printf("Current length:  [%d]\n", len_2);
+
+	/*Test case for empty string input:*/
+	len_1 = printf("Expected output: %s\n", "");
+	len_2 = _printf("Current output:  %R\n", "");
+	printf("Expected length: [%d]\n", len_1);
+	printf("Current length:  [%d]\n", len_2);
+
+	/*Test case for lowercase string input:*/
+	len_1 = printf("Expected output: %s\n", "hello");
+	len_2 = _printf("Current output:  %R\n", "hello");
+	printf("Expected length: [%d]\n", len_1);
+	printf("Current length:  [%d]\n", len_2);
+
+	/*Test case for uppercase string input:*/
+	len_1 = printf("Expected output: %s\n", "WORLD");
+	len_2 = _printf("Current output:  %R\n", "WORLD");
+	printf("Expected length: [%d]\n", len_1);
+	printf("Current length:  [%d]\n", len_2);
+
+	/*Test case for mixed case string input:*/
+	len_1 = printf("Expected output: %s\n", "HeLLo WoRLd");
+	len_2 = _printf("Current output:  %R\n", "HeLLo WoRLd");
+	printf("Expected length: [%d]\n", len_1);
+	printf("Current length:  [%d]\n", len_2);
+
+
 	return (0);
 }
